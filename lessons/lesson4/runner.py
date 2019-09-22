bb0582a3ca80dafd87989fac4e5ae3e0acdd0139
Dmitry Zverkov <dzverkov@gmail.com>
2019-09-22 09:59:17 +0300

    les 4

---
+++ lessons/lesson4/runner.py
@@ -0,0 +1,23 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+from scrapy.crawler import CrawlerProcess
+from scrapy.settings import Settings
+
+from jobparser import settings
+from jobparser.spiders.hhru import HhruSpider
+from jobparser.spiders.instagram import InstagramSpider
+
+do_env = join(dirname(__file__), '.env')
+load_dotenv(do_env)
+
+INST_LOGIN = os.getenv('INST_LOGIN')
+INST_PWD = os.getenv('INST_PASSWORD')
+
+if __name__ == '__main__':
+    crawler_settings = Settings()
+    crawler_settings.setmodule(settings)
+    process = CrawlerProcess(settings=crawler_settings)
+    #process.crawl(HhruSpider)
+    process.crawl(InstagramSpider, ['geekbrains', 'harleydavidson'], INST_LOGIN, INST_PWD)
+    process.start()
